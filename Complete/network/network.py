0a92f78e2c44749d86c7cfd271e0d9b26238a12b
gwjm <gregor@the-macdonalds.net>
2024-02-07 18:26:36 -0800

    Refactoring

---
+++ Complete/network/network.py
@@ -0,0 +1,17 @@
+from utils.encoder import Q_Sampler
+import torch.nn, torch.nn.functional, torch.optim as nn, F, optim
+
+# Sampler Abstraction
+# Forward, backward, and sampling in a single model.
+# Model is taking the noise and predicting the specific noise.
+# If we can learn the mean we can reparameterize to find the exact the noid
+
+
+class Model_Wrapper(nn.Module):
+    def __init__(
+        self, beta_schedule: torch.Tensor, max_timesteps: torch.Tensor
+    ) -> None:
+        self.Q = Q_Sampler(beta_schedule, max_timesteps)
+
+    def q_sample_image(self, image: torch.Tensor):
+        return self.Q(image)
