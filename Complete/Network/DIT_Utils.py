ef552a0b29a36259a60d3140a700f867b12ffd34
gwjm <gregor@the-macdonalds.net>
2024-02-15 17:06:35 -0800

    Add PatchEmbed, TimestepEmbedder, and LabelEmbedder classes

--- Complete/Network/DIT_Utils.py
+++ Complete/Network/DIT_Utils.py
@@ -1,21 +1,56 @@
 import torch.nn as nn
+import torch
 from torch import Tensor
+import math
 
 
-class Patchify(nn.Module):
+class PatchEmbed(nn.Module):
     def __init__(self, patch_size: int) -> None:
-        super(Patchify, self).__init__()
-        self.patch_size = patch_size
+        super(PatchEmbed, self).__init__()
+        pass
 
     def forward(self, x: Tensor) -> Tensor:
-        batch_size, n_channels, height, width = x.shape
-        assert (
-            height % self.patch_size == 0 and width % self.patch_size == 0
-        ), "Image size must be divisible by the patch size"
-        patches = x.unfold(2, self.patch_size, self.patch_size).unfold(
-            3, self.patch_size, self.patch_size
+        pass
+
+
+class TimestepEmbedder(nn.Module):
+    def __init__(self, hidden_dim: int, freq_embedding_dim: int) -> None:
+        super(TimestepEmbedder, self).__init__()
+        self.mlp = nn.Sequential(
+            nn.Linear(freq_embedding_dim, hidden_dim),
+            nn.SiLU(),
+            nn.Linear(hidden_dim, hidden_dim),
         )
-        patches = patches.permute(0, 2, 3, 1, 4, 5).reshape(
-            batch_size, -1, n_channels, self.patch_size, self.patch_size
-        )
-        return patches
+        self.embedding_dim = freq_embedding_dim
+
+    @staticmethod
+    def get_timestep_embedding(
+        self, t: Tensor, out_dim: int, max_period: int
+    ) -> Tensor:
+        half = out_dim // 2
+        freqs = torch.exp(
+            -math.log(max_period)
+            * torch.arange(start=0, end=half, dtype=torch.float32)
+            / half
+        ).to(device=t.device)
+        args = t[:, None].float() * freqs[None]
+        embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
+        if out_dim % 2:
+            embedding = torch.cat(
+                [embedding, torch.zeros_like(embedding[:, :1])], dim=-1
+            )
+        return embedding
+
+    def forward(self, x: Tensor) -> Tensor:
+        freq_embedding = self.get_timestep_embedding(x, self.freq_embedding_dim)
+        return self.mlp(freq_embedding)
+
+
+class LabelEmbedder(nn.Module):
+    def __init__(self, n_classes: int, embedding_dim) -> None:
+        super(LabelEmbedder, self).__init__()
+        self.embedding_table = nn.Embedding(n_classes, embedding_dim)
+        self.n_classes = n_classes
+
+    def forward(self, x: Tensor) -> Tensor:
+        return self.embedding_table(x)
